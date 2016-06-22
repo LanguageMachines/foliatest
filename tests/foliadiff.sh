4ba968665fdb379504c27a8888ae7e004c491bf5
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2016-06-22 12:29:52 +0200

    not ideal, but better

--- tests/foliadiff.sh
+++ tests/foliadiff.sh
@@ -1,8 +1,21 @@
 # /bin/sh -x
-
-if [ "$my_bin" = "" ];
+if [ "$exe" = "" ]
 then
-    my_bin="/usr/local/bin"
+    exe="$HOME/usr/local/bin/folialint"
+    if [ ! -f $exe ]
+    then
+	exe="/exp/sloot/usr/local/bin/folialint"
+	if [ ! -f $exe ]
+	then
+	    exe="/usr/local/bin/folialint"
+	fi
+    fi
+fi
+
+if [ "$exe" = "" ];
+then
+    "cannot locate folialint"
+    exit 1
 fi
 
 if [ $# -ne 2 ]
@@ -13,8 +26,8 @@ fi
 
 t1=/tmp/${1##*/}.s1
 t2=/tmp/${2##*/}.s2
-$my_bin/folialint --strip "$1" > $t1
-$my_bin/folialint --strip "$2" > $t2
+$exe --strip "$1" > $t1
+$exe --strip "$2" > $t2
 xmldiff $t1 $t2
 if [ $? -ne 0 ]
 then
