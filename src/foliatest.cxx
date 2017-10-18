f2207c034228e77fd04f2dbabb459e87763299a9
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2017-10-18 16:56:59 +0200

    modified test

--- src/foliatest.cxx
+++ src/foliatest.cxx
@@ -3897,8 +3897,11 @@ void text_test13f(){
   w = new Word( args, &doc );
   s->append( w );
 
-  assertEqual( doc.xmlstring(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-"<FoLiA xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://ilk.uvt.nl/folia\" xml:id=\"example\" generator=\"libfolia-v1.10\" version=\"1.5.0\"><metadata type=\"native\"><annotations/></metadata><text xml:id=\"example.text.1\"><s xml:id=\"example.s.1\"><t>De site staat online . </t><w xml:id=\"example.s.1.w.1\"><t>De</t></w><w xml:id=\"example.s.1.w.2\"><t>site</t></w><w xml:id=\"example.s.1.w.3\"><t>staat</t></w><w xml:id=\"example.s.1.w.4\"><t>online</t></w><w xml:id=\"example.s.1.w.5\"><t>.</t></w></s></text></FoLiA>\n" );
+  doc.setmode("strip");
+  assertEqual( doc.getmode(), "mode=strip,checktext," );
+  assertEqual( doc.xmlstring(),
+	       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+"<FoLiA xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns=\"http://ilk.uvt.nl/folia\" xml:id=\"example\" generator=\"\" version=\"\"><metadata type=\"native\"><annotations/></metadata><text xml:id=\"example.text.1\"><s xml:id=\"example.s.1\"><t>De site staat online . </t><w xml:id=\"example.s.1.w.1\"><t>De</t></w><w xml:id=\"example.s.1.w.2\"><t>site</t></w><w xml:id=\"example.s.1.w.3\"><t>staat</t></w><w xml:id=\"example.s.1.w.4\"><t>online</t></w><w xml:id=\"example.s.1.w.5\"><t>.</t></w></s></text></FoLiA>\n" );
 }
 
 void text_test14(){
