79c4c52f316a83995b9297d14da88696fa66093b
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-10-11 12:59:28 +0200

    oesp

--- tests/foliadiff.sh
+++ tests/foliadiff.sh
@@ -26,8 +26,8 @@ fi
 
 t1=/tmp/${1##*/}.s1
 t2=/tmp/${2##*/}.s2
-$exe --strip --KANON "$1" -o $t1
-$exe --strip --KANON "$2" -o $t2
+$exe --strip --KANON "$1" --output $t1
+$exe --strip --KANON "$2" --output $t2
 diff $t1 $t2
 if [ $? -ne 0 ]
 then
