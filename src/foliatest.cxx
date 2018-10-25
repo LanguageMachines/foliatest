35e72cf2577e21f7c3af0336d6d2a73704d40c14
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2018-10-25 17:06:39 +0200

    start adding a new test

--- src/foliatest.cxx
+++ src/foliatest.cxx
@@ -5127,6 +5127,27 @@ void processor_test008c(){
   }
 }
 
+void processor_test008d(){
+  startTestSerie( " process a document searching for text nodes, flushing in between " );
+  TextProcessor proc( "tests/folia-head.xml", "/tmp/proctest-8d.xml");
+  proc.set_debug(true);
+  ofstream os( "/tmp/proctest-8d.out" );
+  if ( proc.ok() ){
+    proc.setup("",true);
+    FoliaElement *e = 0;
+    while ( (e = proc.next_text_parent() ) ){
+      os << e->id() << " : " << e->str() << endl;
+    }
+    proc.finish();
+    int stat = system( "diff /tmp/proctest-8d.out tests/proctest-8d.ok" );
+    assertMessage( "/tmp/proctest-8d.out tests/proctest-8d.ok differ!",
+     		   (stat == 0) );
+    stat = system( "./tests/foliadiff.sh /tmp/proctest-8d.xml tests/proctest-8d.xml.ok" );
+    assertMessage( "/tmp/proctest-8d.xml tests/proctest-8d.xml.ok differ!",
+     		   (stat == 0) );
+  }
+}
+
 #endif // FOLIA_INT_VERSION >= 120
 
 int main(){
@@ -5360,6 +5381,7 @@ int main(){
   processor_test008a();
   processor_test008b();
   processor_test008c();
+  processor_test008d();
 #endif
   summarize_tests(0);
 }
