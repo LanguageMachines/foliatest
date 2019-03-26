74c3b1bfc4fb18fa043dc382e7114ab2848251c8
Ko van der Sloot <K.vanderSloot@let.ru.nl>
2019-03-26 17:44:21 +0100

    argll

--- src/foliatest.cxx
+++ src/foliatest.cxx
@@ -39,12 +39,12 @@
 #include <unicode/unistr.h>
 #include "libxml/tree.h"
 #include "ticcutils/StringOps.h"
-#include "ticcutils/PrettyPrint.h"
-#include "ticcutils/XMLtools.h"
-#include "libfolia/folia.h"
 template< typename T >
 inline std::ostream& TiCC::operator<< ( std::ostream& , const std::vector<T>& );
 #include "ticcutils/UnitTest.h"
+#include "ticcutils/PrettyPrint.h"
+#include "ticcutils/XMLtools.h"
+#include "libfolia/folia.h"
 
 #include "config.h"
 using namespace std;
