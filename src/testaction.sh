674c0d47b44934330e076fa2c16307491fb23bc6
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2021-08-30 12:22:54 +0200

    first attempt for an test_action script

---
+++ src/testaction.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Running testaction script!"
+export FOLIAPATH=foliapy/folia-repo
+export EXPECT=0
+LD_LIBRARY_PATH: $LD_LIBRARY_PATH:/usr/local/lib ./testall.sh
+TEST_STAT=$?
+echo $TEST_STAT > status.tmp
+exit 0
