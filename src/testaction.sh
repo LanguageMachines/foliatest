08cd91646246529461ead8fca8cbeacc77a37400
Ko van der Sloot <Ko.vanderSloot@ziggo.nl>
2021-08-30 14:59:05 +0200

    some refactoring

--- src/testaction.sh
+++ src/testaction.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo "Running testaction script!"
-export FOLIAPATH=foliapy/folia-repo
-export EXPECT=0
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib ./src/foliatest
+./src/foliatest
 TEST_STAT=$?
 echo $TEST_STAT > status.tmp
 exit 0
